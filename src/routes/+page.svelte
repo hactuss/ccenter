298a111f919703daf19c80f79a47fe5a625f148b
hactuss <hactuss@proton.me>
2025-09-27 18:41:34 +0200

    modified:   src/app.css 	new file:   src/lib/assets/external.png 	new file:   src/lib/assets/favicon.png 	modified:   src/routes/+layout.svelte 	modified:   src/routes/+page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
 <script>
-    let ew = 1;
-    let eh = 1;
-    let cw = 1;
-    let ch = 1;
-    let center_x = 2;
-    let center_y = 2;
+    let ew = 0;
+    let eh = 0;
+    let cw = 0;
+    let ch = 0;
+    let center_x = 0;
+    let center_y = 0;
 
     //presets
     function pre_A4() {
@@ -17,21 +17,16 @@
         ch = 1080;
         return 1;
     }
-    let dropdownmenu = "block";
-    function changedropdown() {
-        switch (dropdownmenu) {
-            case "none":
-                dropdownmenu = "block";
-                break;
-            case "block":
-                dropdownmenu = "none";
-                break;
-        }
+    function pre_HD() {
+        cw = 1280;
+        ch = 720;
     }
 </script>
 
-<div class="">
-    <h1>Center Calculator</h1>
+<div id="main_container">
+    <nav class="flex">
+        <h2>Center calculator for graphic design</h2>
+    </nav>
     <div class="flex">
         <div class="flex flex-col w-[100%]">
             <div>
@@ -52,15 +47,8 @@
             </div>
         </div>
         <div class="w-[100%]">
-            <button
-                class=""
-                onclick={() => {
-                    changedropdown();
-                }}
-            >
-                <h2>presets</h2>
-            </button>
-            <div style="display: {dropdownmenu}; border: solid black;">
+            <h3>preset canvas resolutions</h3>
+            <div>
                 <button
                     onclick={() => {
                         pre_A4();
@@ -71,16 +59,32 @@
                         pre_FullHD();
                     }}>Full HD</button
                 >
+                <button
+                    onclick={() => {
+                        pre_HD();
+                    }}>HD</button
+                >
             </div>
         </div>
     </div>
     <button
+        id="calculate"
         onclick={() => {
             center_x = cw / 2 - ew / 2;
             center_y = ch / 2 - eh / 2;
         }}>calculate</button
     >
-    <h1>X: {center_x}</h1>
-    <h1>Y: {center_y}</h1>
+    <h1>X: {center_x} px</h1>
+    <h1>Y: {center_y} px</h1>
+    <div>
+        <a href="https://hactuss.vercel.app">made by hactuss</a>
+        <a href="https://github.com/hactuss/ccenter">Source code</a>
+    </div>
 </div>
-<p>by hactuss</p>
+
+<style>
+    #calculate {
+        background-color: darkblue;
+        color: white;
+    }
+</style>
