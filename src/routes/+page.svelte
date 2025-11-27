196d6cdd0f06b76803b5760d36676ca41c781c6d
hactuss <hactuss@proton.me>
2025-11-27 21:40:16 +0100

    modified:   deno.lock 	modified:   package.json 	modified:   src/app.css 	modified:   src/routes/+page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -39,13 +39,15 @@
         return ((center_x = cw / 2 - ew / 2), (center_y = ch / 2 - eh / 2));
     }
 </script>
-<div>
+
+<!--
+<main id="MAIN">
     <main id="center-calc">
         <nav>
             <div><h2>Center calculator for graphic design</h2></div>
         </nav>
         <div class="flex">
-            <div class="flex flex-col w-[100%]">
+            <div class="flex flex-col">
                 <div>
                     <label for="">canvas width</label>
                     <input
@@ -95,7 +97,7 @@
             onclick={() => {
                 calc();
             }}>calculate</button
-        > -->
+        >
         <h1>X: {center_x} px</h1>
         <h1>Y: {center_y} px</h1>
         <div>
@@ -103,7 +105,7 @@
             <a href="https://github.com/hactuss/ccenter">Source code</a>
         </div>
     </main>
-    
+
     <br />
     <main id="ratio-calc" style="display:hidden">
         <h2>Ratio calc</h2>
@@ -118,7 +120,7 @@
                     bind:value={length}
                     onchange={calcratio}
                 />
-    
+
                 <p>{calcratio}</p>
             </div>
             <div>
@@ -130,10 +132,88 @@
             </div>
         </div>
     </main>
-</div>
+</main>
+-->
+<main id="newMain">
+    <div id="titlebar">
+        <h2>Center Calculator for graphic design</h2>
+        <a href="https://hactuss.vercel.app">made by hactuss</a>
+        <a href="https://github.com/hactuss/ccenter">Source code</a>
+    </div>
+    <section>
+        <div id="buttonholder">
+            <div class="flex flex-col" id="inputs">
+                <div class="flex">
+                    <div>
+                        <label for="">canvas width</label>
+                        <input type="number" bind:value={cw} oninput={calc} />
+                    </div>
+                    <div>
+                        <label for="">canvas height</label>
+                        <input type="number" bind:value={ch} oninput={calc} />
+                    </div>
+                </div>
+
+                <div class="flex">
+                    <div>
+                        <label for="">element width</label>
+                        <input type="number" bind:value={ew} oninput={calc} />
+                    </div>
+                    <div>
+                        <label for="">element height</label>
+                        <input type="number" bind:value={eh} oninput={calc} />
+                    </div>
+                </div>
+            </div>
+            <div class="w-[100%]" id="presets">
+                <h3>preset canvas resolutions</h3>
+                <div>
+                    <button onclick={pre_A4}>A4 [2480 x 3508]</button>
+                    <button onclick={pre_FullHD}>Full HD [1920 x 1080]</button>
+                    <button onclick={pre_HD}>HD [1280 x 720]</button>
+                </div>
+            </div>
+        </div>
+        <!--
+        <button
+            id="calculate"
+            onclick={() => {
+                calc();
+            }}>calculate</button
+        > -->
+        <h1>X: {center_x} px</h1>
+        <h1>Y: {center_y} px</h1>
+    </section>
+</main>
+
 <style>
-    #calculate {
-        background-color: darkblue;
+    #MAIN {
+        background-color: rgb(50 50 50);
+    }
+    #newMain {
+        border: 2px solid white;
+        border-radius: 0.2rem;
+
+        background: rgb(0 0 0);
+    }
+    #titlebar {
+        font-family: monospace;
         color: white;
+        background: #141414;
+        background: linear-gradient(
+            0deg,
+            rgba(20, 20, 20, 1) 0%,
+            rgba(50, 50, 50, 1) 50%,
+            rgba(100, 100, 100, 1) 100%
+        );
+        border: solid black 3px;
+        border-radius: 3px;
+    }
+    section {
+        color: white;
+    }
+
+    #buttonholder {
+        display: flex;
     }
 </style>
